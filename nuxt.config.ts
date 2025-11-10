15a5ecc45f0415b839dc742062d18b53ea0af624
Kévin Morpain <kevmorpain@gmail.com>
2025-11-10 01:31:48 +0100

    add zod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/test-utils', '@nuxt/ui', '@nuxthub/core', '@vite-pwa/nuxt', '@nuxtjs/i18n'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/test-utils', '@nuxt/ui', '@nuxthub/core', '@vite-pwa/nuxt', '@nuxtjs/i18n', 'nuxt-zod-i18n'],
 
   pages: true,
 
